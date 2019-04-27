31f97ea770a13b5319414167bc6417826065dd37
Jacqueline Gwynn <jacquelinegwynn@jacquelines-air.attlocal.net>
2019-04-27 09:25:02 -0400

    update Article and Note files format

--- models/Note.js
+++ models/Note.js
@@ -6,8 +6,9 @@ const Schema = mongoose.Schema;
 // Using the Schema constructor, create a new NoteSchema object
 // This is similar to a Sequelize model
 const NoteSchema = new Schema({
-  title: String,
-  body: String
+  body: {
+    type: String
+  }
 });
 
 // This creates our model from the above schema, using mongoose's model method

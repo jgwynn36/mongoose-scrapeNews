9368240048aec32d41b0fb34c0acc4552351bb4a
Jacqueline Gwynn <jacquelinegwynn@jacquelines-air.attlocal.net>
2019-04-28 17:45:31 -0400

    add getJSOn method and update html to show article name

--- public/app.js
+++ public/app.js
@@ -34,7 +34,7 @@ $(document).on("click", "p", function () {
     .then(data => {
       console.log(data);
       // The title of the article
-      $("#notes").append("<h2>" + data.headline + "</h2>");
+      $("#notes").append("<h2>" + data.title + "</h2>");
       // An input to enter a new title
       $("#notes").append("<input id='titleinput' name='title' >");
       // A textarea to add a new note body

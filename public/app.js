02f49b6b3821560904316abb6dfdf1fa0a1b3977
Jacqueline Gwynn <jacquelinegwynn@jacquelines-air.attlocal.net>
2019-04-28 17:50:18 -0400

    add Summary to p tag

--- public/app.js
+++ public/app.js
@@ -3,7 +3,7 @@ $.getJSON("/articles", data => {
   // For each one
   for (let i = 0; i < data.length; i++) {
     // Display the apropos information on the page
-    $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+    $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "<br/>" + data[i].Summary + "</p>");
   }
 });
 
